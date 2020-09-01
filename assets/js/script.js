45e3bcf7a4a8ac22965d4e9ebf5d3f1ca6444eb8
TolgaAkaMoon <4160141@RTS-365.dk>
2020-09-01 13:24:26 +0200

    added browse section and add javascript & html

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,37 @@ Notification.requestPermission(function(status){
     });
   }
 })
+
+
+
+
+
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var Base64 = "basic ZjFlYmQ5YmRjMTVlNDhlMGIxYmNmZjhiNmY0NmNjZjA6ZTFmZjNkNDhjMDRiNGIzNGFjYzZkMmYyMGQxYmNjMDY=";
+    fetch('https://accounts.spotify.com/api/token', {
+      method: 'post',
+      body: "grant_type=client_credentials",
+      headers: {
+        'Authorization': Base64,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }).then(function (res) {
+      return res.json();
+    }).then(function (json) {
+      var accessToken = "Bearer " + json.access_token;
+      fetch('https://api.spotify.com/v1/playlists/5vjPjAUsOcqUNas90acSMg', {
+        method: 'get',
+        headers: {
+          'Authorization': accessToken
+        }
+      }).then(function (res) {
+        return res.json();
+      }).then(function (tracks) {
+        tracks.tracks.items.forEach(function (produkt) {
+          document.querySelector(".main").innerHTML += "\n                <div class=\"main__container\">\n                  <a class=\"container__anker\" href=\"/player/?track=".concat(produkt.track.id, "\">\n                    <img src=\"").concat(produkt.track.album.images[0].url, "\" class=\"container__background\">\n                    <h2 class=\"container__title\">").concat(produkt.track.name, "</h2>\n                    <h5 class=\"container__desc\">").concat(produkt.track.artists[0].name, "</h5>\n                    <img src=\"../assets/images/Footer-shadow.png\" class=\"container__img\">\n                  </a>\n                </div>\n                ");
+        });
+      });
+    });
+  });
