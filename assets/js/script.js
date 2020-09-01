d2ca1180c4c35ac1d1acc1c518bc1cb2674b0690
TolgaAkaMoon <4160141@RTS-365.dk>
2020-09-01 13:29:28 +0200

    fix browse section

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,30 +52,44 @@ Notification.requestPermission(function(status){
 
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    var Base64 = "basic ZjFlYmQ5YmRjMTVlNDhlMGIxYmNmZjhiNmY0NmNjZjA6ZTFmZjNkNDhjMDRiNGIzNGFjYzZkMmYyMGQxYmNjMDY=";
-    fetch('https://accounts.spotify.com/api/token', {
+document.addEventListener("DOMContentLoaded", () => {
+
+  let Base64 = "basic ZjFlYmQ5YmRjMTVlNDhlMGIxYmNmZjhiNmY0NmNjZjA6ZTFmZjNkNDhjMDRiNGIzNGFjYzZkMmYyMGQxYmNjMDY=";
+
+  fetch('https://accounts.spotify.com/api/token', {
       method: 'post',
       body: "grant_type=client_credentials",
       headers: {
-        'Authorization': Base64,
-        'Content-Type': 'application/x-www-form-urlencoded'
+          'Authorization': Base64,
+          'Content-Type': 'application/x-www-form-urlencoded'
       }
-    }).then(function (res) {
-      return res.json();
-    }).then(function (json) {
-      var accessToken = "Bearer " + json.access_token;
+  })
+  .then(res => res.json())
+  .then(json => {
+
+  let accessToken = "Bearer "+ json.access_token;
+
       fetch('https://api.spotify.com/v1/playlists/5vjPjAUsOcqUNas90acSMg', {
-        method: 'get',
-        headers: {
-          'Authorization': accessToken
-        }
-      }).then(function (res) {
-        return res.json();
-      }).then(function (tracks) {
-        tracks.tracks.items.forEach(function (produkt) {
-          document.querySelector(".main").innerHTML += "\n                <div class=\"main__container\">\n                  <a class=\"container__anker\" href=\"/player/?track=".concat(produkt.track.id, "\">\n                    <img src=\"").concat(produkt.track.album.images[0].url, "\" class=\"container__background\">\n                    <h2 class=\"container__title\">").concat(produkt.track.name, "</h2>\n                    <h5 class=\"container__desc\">").concat(produkt.track.artists[0].name, "</h5>\n                    <img src=\"../assets/images/Footer-shadow.png\" class=\"container__img\">\n                  </a>\n                </div>\n                ");
-        });
-      });
-    });
+          method: 'get',
+          headers: {
+              'Authorization': accessToken,
+          }
+      })
+      .then(res => res.json())
+      .then(tracks => {
+          console.log(tracks.tracks.items)
+          tracks.tracks.items.forEach(produkt => {
+              console.log(produkt.track)
+              document.querySelector(".main").innerHTML += `
+              <div class="main__container">
+                <a class="container__anker" href="/player/?track=${produkt.track.id}">
+                  <img src="${produkt.track.album.images[0].url}" class="container__background">
+                  <h2 class="container__title">${produkt.track.name}</h2>
+                  <h5 class="container__desc">${produkt.track.artists[0].name}</h5>
+                </a>
+              </div>
+              `
+          })
+      })
   });
+});
